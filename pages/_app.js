57f0f250c0a127d46907ee067739ad0794fe5a38
Durph09 <105114600+Durph09@users.noreply.github.com>
2023-07-26 13:20:58 -0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
+import { UserContext } from "@/lib/context";
+
+import { useUserData } from "@/lib/hooks";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const userData = useUserData();
+  console.log('_app .js 11 userData:', userData)
+  
+  return (
+    <UserContext.Provider value={{userData}}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  );
 }
